1095209491f6f179362f62479adcf74076457620
ashot.barkhudaryan <ashot.barkhudaryan@betconstruct.com>
2019-07-26 14:14:57 +0400

    final working version

--- js/index.js
+++ js/index.js
@@ -5,14 +5,20 @@ const FPS = 60;
 let duration = 20;
 const step = 1 / (FPS);
 let BallTrackSprite;
-let ShouldSpin = true;
+let NumberSprite;
 let time = 0;
-const NUMBER_COUNT = 36;
-const DesiredNumber = 3;
-const SpinCount = 1;
+let DesiredNumber = 18;
+const SpinCount = 10;
+let Offset = 0;
+const GameState = {
+    idle: 1,
+    spinning: 2
+};
 
-const Angle = 0.1745;
-const NumberSequence = [6,21,13,36,11,30,8,23,5,24,18,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26,0,32,15,19,4,21,2,25,17,34];
+let CurrentGameState = GameState.idle;
+
+const SingleNumberAngle = -0.1698;
+const NumberSequence = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
 
 function lerp(a, b, t) {
     return (1 - t) * a + t * b;
@@ -26,22 +32,14 @@ function easeOutQuart(t) {
     return 1 - Math.pow(1 - t, 4);
 }
 
-function clamp(value, min, max) {
-    if (value < min) return min;
-    if (value > max) return max;
-    return value;
+class Roulette {
+    static Spin(time) {
+        if (!BallTrackSprite) return;
+
+        BallTrackSprite.material.rotation = -lerp(Offset, Math.PI * 2 * SpinCount + NumberSequence.indexOf(DesiredNumber) * SingleNumberAngle, easeOutQuart(time / duration));
+    }
 }
 
-function easeBackout(t) {
-    const s = 1.70158;
-    return t * t * ((s + 1) * t - s);
-}
-
-function RotateObject(t, delta) {
-    t += delta;
-    if (t < 0 || t > 1) t = -t;
-    BallTrackSprite.material.rotation = -lerp(0, Math.PI / 2, ease(t));
-}
 
 class WebGLCore {
     constructor(RendererConfigs, CameraConfigs) {
@@ -83,14 +81,14 @@ class WebGLCore {
             this.scene.add(grid);
         }
 
-        light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(0, 200, 100);
-        light.castShadow = true;
-        light.shadow.camera.top = 180;
-        light.shadow.camera.bottom = -100;
-        light.shadow.camera.left = -120;
-        light.shadow.camera.right = 120;
-        this.scene.add(light);
+        // light = new THREE.DirectionalLight(0xffffff);
+        // light.position.set(0, 200, 100);
+        // light.castShadow = true;
+        // light.shadow.camera.top = 180;
+        // light.shadow.camera.bottom = -100;
+        // light.shadow.camera.left = -120;
+        // light.shadow.camera.right = 120;
+        // this.scene.add(light);
 
         // Window resize callback
         window.addEventListener('resize', () => this.OnWindowResize(), false);
@@ -99,21 +97,15 @@ class WebGLCore {
     animate() {
         requestAnimationFrame(() => this.animate());
 
-        let ElapsedTime = this.clock.getElapsedTime();
-        time += step;
-        if (BallTrackSprite) {
-            if (ShouldSpin) {
-                BallTrackSprite.material.rotation = lerp(0, Math.PI * 2 * SpinCount + NumberSequence.indexOf(DesiredNumber) * Angle, easeOutQuart(time / duration));
-                console.log(ElapsedTime, time / duration);
-            }
-        }
+        if (CurrentGameState === GameState.spinning) {
+            time += step;
+            Roulette.Spin(time);
 
-        if (time > duration) {
-            if (ShouldSpin) {
-                console.log(NumberSequence.indexOf(DesiredNumber) * Angle, DesiredNumber);
-                console.log('Final Rotation: ' + BallTrackSprite.material.rotation);
+            if (time > duration) {
+                CurrentGameState = GameState.idle;
+                time = 0;
+                Offset = BallTrackSprite.material.rotation / (Math.PI * 2);
             }
-            ShouldSpin = false;
         }
 
         this.controls.update();
@@ -129,11 +121,13 @@ class WebGLCore {
     SpriteLoad() {
         this.scene.add(TextureToSpriteLoader.load('Textures/1.png', new THREE.Vector3(240, 240, 1)));
         this.scene.add(TextureToSpriteLoader.load('Textures/2.png', new THREE.Vector3(230, 230, 1)));
-        BallTrackSprite = TextureToSpriteLoader.load('Textures/3.png', new THREE.Vector3(180, 180, 1));
-        // BallTrackSprite.material.rotation = -0.17;
+        BallTrackSprite = TextureToSpriteLoader.load('Textures/test.png', new THREE.Vector3(180, 180, 1));
         this.scene.add(BallTrackSprite);
         this.scene.add(TextureToSpriteLoader.load('Textures/4.png', new THREE.Vector3(245, 245, 1)));
         this.scene.add(TextureToSpriteLoader.load('Textures/5.png', new THREE.Vector3(100, 100, 1)));
+        // NumberSprite = TextureToSpriteLoader.load(('Textures/Num20.png'), new THREE.Vector3(90,90,1));
+        // this.scene.add(TextureToSpriteLoader.load('Textures/Num0.png', new THREE.Vector3(100, 100, 1)));
+        this.scene.add(NumberSprite);
     }
 }
 
@@ -157,17 +151,22 @@ WebGLCoreWrapper.init();
 WebGLCoreWrapper.animate();
 WebGLCoreWrapper.SpriteLoad();
 
-const gui = new dat.GUI({resizable: false});
+const gui = new dat.GUI();
 
 
 let FizzyText = function () {
-    this.explode = function () {
+    this.spin = function () {
         // Spin Roulette
-        ShouldSpin = !ShouldSpin;
+        if (CurrentGameState === GameState.spinning) return;
+        CurrentGameState = GameState.spinning;
+        DesiredNumber = THREE.Math.randInt(0, 36);
+        console.log('Desired Number: ' + DesiredNumber);
+        // let Angle = Math.PI * 2 * SpinCount + NumberSequence.indexOf(DesiredNumber) * SingleNumberAngle
+        // console.log('Desired angle: ' + Angle, Offset, BallTrackSprite.material.rotation);
     };
 };
 
 window.onload = function () {
-    let layers = new FizzyText();
-    gui.add(layers, 'explode');
+    let layers = new FizzyText({resizable: false});
+    gui.add(layers, 'spin');
 };
